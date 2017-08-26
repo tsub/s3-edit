62e04a2947e65744d1b0e38896337c4cc7b7fbc2
tsub <tsubasatakayama511@gmail.com>
2017-08-26 23:27:43 +0900

    Refactor cli.Edit

--- cli/edit.go
+++ cli/edit.go
@@ -1,52 +1,38 @@
 package cli
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	myS3 "github.com/tsub/s3-edit/cli/s3"
 )
 
 // Edit directly a file on S3
 func Edit(path myS3.Path) {
-	sess := session.Must(session.NewSession())
-	svc := s3.New(sess)
+	body := myS3.GetObject(path)
 
-	bucket := aws.String(path.Bucket)
-	key := aws.String(path.Key)
+	tempfilePath := createTempfile(path, body)
+	defer os.Remove(tempfilePath)
 
-	getObjectInput := &s3.GetObjectInput {
-		Bucket: bucket,
-		Key: key,
-	}
-	res, err := svc.GetObject(getObjectInput)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	editedBody := editFile(tempfilePath)
+	myS3.PutObject(path, editedBody)
+}
 
-	buf := new(bytes.Buffer)
-	if _, err := buf.ReadFrom(res.Body); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	body := buf.Bytes()
+func createTempfile(path myS3.Path, body []byte) (tempfilePath string) {
+	tempfilePath = "/tmp/" + path.Key
 
-	tempfilePath := "/tmp/" + *key
 	if err := ioutil.WriteFile(tempfilePath, body, os.ModePerm); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	defer os.Remove(tempfilePath)
 
-	cmd := exec.Command("sh", "-c", "nvim " + tempfilePath)
+	return
+}
+
+func editFile(path string) string {
+	cmd := exec.Command("sh", "-c", "nvim " + path)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
@@ -55,20 +41,11 @@ func Edit(path myS3.Path) {
 		os.Exit(1)
 	}
 
-	changedFile, err := ioutil.ReadFile(tempfilePath)
+	changedFile, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	putBody := string(changedFile[:])
 
-	putObjectInput := &s3.PutObjectInput {
-		Body: aws.ReadSeekCloser(strings.NewReader(putBody)),
-		Bucket: bucket,
-		Key: key,
-	}
-	if _, err := svc.PutObject(putObjectInput); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	return string(changedFile[:])
 }
---
+++ cli/s3/object.go
@@ -0,0 +1,48 @@
+package s3
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var sess = session.Must(session.NewSession())
+var svc = s3.New(sess)
+
+// GetObject download a file on S3
+func GetObject(path Path) []byte {
+	input := &s3.GetObjectInput {
+		Bucket: aws.String(path.Bucket),
+		Key: aws.String(path.Key),
+	}
+	res, err := svc.GetObject(input)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(res.Body); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return buf.Bytes()
+}
+
+// PutObject upload a file to S3
+func PutObject(path Path, body string) {
+	input := &s3.PutObjectInput {
+		Body: aws.ReadSeekCloser(strings.NewReader(body)),
+		Bucket: aws.String(path.Bucket),
+		Key: aws.String(path.Key),
+	}
+	if _, err := svc.PutObject(input); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
