658aa8a4e5b5447cf74d600ee4ac62c20f0eabbc
tsub <tsubasatakayama511@gmail.com>
2017-08-26 23:36:29 +0900

    Refactor validate arguments using cobra.ExactArgs

--- cmd/edit.go
+++ cmd/edit.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 	"github.com/tsub/s3-edit/cli"
 	"github.com/tsub/s3-edit/cli/s3"
@@ -13,12 +10,8 @@ var editCmd = &cobra.Command{
 	Use:   "edit [S3 file path]",
 	Short: "Edit directly a file on S3",
 	Long: "Edit directly a file on S3",
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			fmt.Println("required 1 argument")
-			os.Exit(1)
-		}
-
 		path := s3.ParsePath(args[0])
 		cli.Edit(path)
 	},
