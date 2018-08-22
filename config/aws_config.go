c642ea7c00233e3ed121ac393aaf1247fa0124bd
koooge <koooooge@gmail.com>
2018-08-22 17:20:45 +0900

    Treat profile settings by aws-sdk-go

--- config/aws_config.go
+++ config/aws_config.go
@@ -1,11 +1,6 @@
 package config
 
 import (
-	"fmt"
-	"os/user"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
@@ -16,19 +11,9 @@ type AWSParams struct {
 
 // NewAWSParams creates a new AWSParams object
 func NewAWSParams(awsProfile string) (*AWSParams, error) {
-	var creds *credentials.Credentials
-
-	if awsProfile != "" {
-		currentUser, err := user.Current()
-		if err != nil {
-			return nil, err
-		}
-
-		creds = credentials.NewSharedCredentials(fmt.Sprintf("%s/.aws/credentials", currentUser.HomeDir), awsProfile)
-	}
-
-	sess := session.Must(session.NewSession(&aws.Config{
-		Credentials: creds,
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Profile:           awsProfile,
+		SharedConfigState: session.SharedConfigEnable,
 	}))
 
 	return &AWSParams{
